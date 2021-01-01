33dca5f670b44aa108f6b53ebbb83cd262bf6e21
hphari12 <76843258+hphari12@users.noreply.github.com>
2021-01-01 22:05:29 +0530

    Update commands.py

--- commands.py
+++ commands.py
@@ -1,16 +1,16 @@
 #import as library
-import code1 
+import code 
 #key and value with no expTime
-code1.create("A",{"AAA":1})
-code1.create("B",{"BBB":2})
+code.create("A",{"AAA":1})
+code.create("B",{"BBB":2})
 #key,value and expireTime
-code1.create("C",{"CCC":3},10)
-code1.create("D",{"DDD":4},1)
+code.create("C",{"CCC":3},10)
+code.create("D",{"DDD":4},1)
 #Read operation
-code1.read("A")
-code1.read("B")
+code.read("A")
+code.read("B")
 #delete operation
-code1.delete("A")
+code.delete("A")
 #for multithreading
 #t1=Thread(target=function,args=arguments)
 t1=Thread(target=(create or read or delete),args=(key_name,value,timeout))
